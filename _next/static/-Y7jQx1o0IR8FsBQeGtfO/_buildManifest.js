3ed67c3455b376ad67768c2322d1a6b6fe041a47
YuutaTsubasa <81959102+YuutaTsubasa@users.noreply.github.com>
2021-09-05 17:42:05 +0000

    Deploying to gh-pages from @ YuutaTsubasa/yuutatsubasa.github.io@1baec3cf70d480e14a097f4b97f220d7c5d9fda4 🚀

---
+++ _next/static/-Y7jQx1o0IR8FsBQeGtfO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3949ef07e270089e69d4.css","static/chunks/pages/index-48dd8db55119de9d9891.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
