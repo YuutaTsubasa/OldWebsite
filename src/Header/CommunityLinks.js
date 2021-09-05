b8cacff2105adcfb55ffd03e4003f24f096baf69
Yuuta Tsubasa <yuuta.tsubasa.knight@gmail.com>
2021-09-05 16:25:40 +0800

    Add header

---
+++ src/Header/CommunityLinks.js
@@ -0,0 +1,12 @@
+function CommunityLinks() {
+    return (
+        <div className="communityLinks">
+            <ul>
+               <li><a href=""></a></li> 
+            </ul>
+        </div>
+    );
+  }
+  
+  export default CommunityLinks;
+  
