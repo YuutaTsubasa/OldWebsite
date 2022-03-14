a8d822b45099cae86b3dd34d5e8846ed427fb17e
悠太翼 <81959102+YuutaTsubasa@users.noreply.github.com>
2022-03-14 12:20:53 +0800

    Fix back App to not use Document

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import Document from '../components/document';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <Document>
-            <Component {...pageProps} />
-        </Document>
-    );
+    return <Component {...pageProps} />;
 }
