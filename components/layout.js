3be4a5bddfaefecdd8582cca86204d8d494130bb
悠太翼 <81959102+YuutaTsubasa@users.noreply.github.com>
2022-03-14 12:20:19 +0800

    Fix back the head section to Layout

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,15 @@ export default function Layout({children, subURL, title, communityLinksData}) {
     return (
         <div className={styles.layout}>
             <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="/fontawesome/css/all.css" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Lexend+Zetta:wght@800&family=Noto+Sans+SC:wght@500&family=Noto+Sans+TC:wght@500&display=swap" rel="stylesheet" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@YuutaTsubasa"/>
+                <meta name="twitter:creator" content="@YuutaTsubasa"/>
+                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
                 <meta property="og:title" content={siteTitle} />
                 <meta property="og:description" content={description} />
                 <meta property="og:url" content={`${baseURL}/${subURL}`} />
