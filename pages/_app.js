b4b2b2802a78999e48fe6bceefe04f5c4aa7c926
悠太翼 <81959102+YuutaTsubasa@users.noreply.github.com>
2022-03-14 12:10:02 +0800

    Update App with Document

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import Document from '../components/document';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <Document>
+            <Component {...pageProps} />
+        </Document>
+    );
 }
